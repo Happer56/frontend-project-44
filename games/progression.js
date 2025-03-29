f0c93db8e9691d97e2e414ddeb7e0fd22ed2fb7e
Ruslan Galeev <happer1337@yandex.ru>
2025-03-29 17:14:04 +0500

    update game

--- games/progression.js
+++ games/progression.js
@@ -5,13 +5,13 @@ const gameProgres = () => {
   const num1 = random(0, 100);
   const num2 = random(1, 10);
 
-  const massiv = progres(num1, num2);
+  const array = progres(num1, num2);
 
-  const qwe = `${".."}`;
-  const elem = random(0, 9);
-  const correctAnswer = String(massiv[elem]);
-  massiv.splice(elem, 1, qwe);
-  const question = massiv;
+  const replacement = `${".."}`;
+  const element = random(0, 9);
+  const correctAnswer = String(array[element]);
+  array.splice(element, 1, replacement);
+  const question = array;
 
   return [question, correctAnswer];
 };
