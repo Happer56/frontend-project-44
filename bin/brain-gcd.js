cf6f770da0efd2742b8bad21dae90b58f07c1b0e
Happer56 <125179519+Happer56@users.noreply.github.com>
2025-04-03 14:54:47 +0500

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,8 +1,1 @@
-#!/usr/bin/env node
 
-import beginGame from '../src/index.js'
- import gameGcd from '../games/gcd.js';
- 
- const rules = "Find the greatest common divisor of given numbers.";
- const startGcd = () => beginGame(rules, gameGcd);
-startGcd();
