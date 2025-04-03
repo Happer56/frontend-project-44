f24f4242b51091493c4ecbb540102ebc41e11d2e
Ruslan Galeev <happer1337@yandex.ru>
2025-04-03 21:09:42 +0500

    add

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node  
 import readlineSync from 'readline-sync';
-
-const nameUser = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?');
+console.log("Welcome to the Brain Games!");
+const nameUser = readlineSync.question('May I have your name?');
 console.log(`${'Hello'} ${nameUser} ${'!'}`);
 export default nameUser;
