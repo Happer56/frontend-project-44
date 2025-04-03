8f01ac3c1b2e7f042fbfc7d896d5dedb8f8be12d
Happer56 <125179519+Happer56@users.noreply.github.com>
2025-04-03 15:39:29 +0500

    Update progression.js

--- games/progression.js
+++ games/progression.js
@@ -2,10 +2,10 @@ import random from "../randomNum.js";
 import progres from "../addMassiv.js";
 
 const gameProgres = () => {
-  const num1 = random(0, 100);
-  const num2 = random(1, 10);
+  const number1 = random(0, 100);
+  const number2 = random(1, 10);
 
-  const array = progres(num1, num2);
+  const array = progres(number1, number2);
 
   const replacement = `${".."}`;
   const element = random(0, 9);
