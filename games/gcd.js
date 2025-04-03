4741ccb305b880e7db7ce23a59b2a797cd3a9947
Happer56 <125179519+Happer56@users.noreply.github.com>
2025-04-03 15:07:12 +0500

    Update gcd.js

--- games/gcd.js
+++ games/gcd.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 import random from "../randomNum.js";
 
-const gcd = (a, b) => ((a % b) ? gcd(b, a % b) : Math.abs(b));
+ const gcd = (a, b) => {
+   if (b === 0) {
+     return a;
+   }
+   return gcd(b, a % b);
+ };
 
 const gameGcd = () => {
   const num1 = random(0, 100);
   const num2 = random(0, 100);
-  const question = `${num1}  ${num2}`;
+  const question = `${num1} ${num2}`;
   const correctAnswer = String(gcd(num1, num2));
   return [question, correctAnswer];
 };
