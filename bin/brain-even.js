8bfd6a83ecaf9a42e71da0fb8ce029b9f4f0091d
Ruslan Galeev <happer1337@yandex.ru>
2025-04-03 19:33:10 +0500

    add

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,6 +2,7 @@
 import beginGame from '../src/index.js';
 import gameEven from '../games/even.js';
 
-const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const startEven = () => beginGame(rules, gameEven);
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 startEven();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const nameUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${name}!`);
-    return name;
-  };
+console.log('Welcome to the Brain Games!');
+const nameUser = readlineSync.question('May I have your name?');
+console.log('Hello, ' + nameUser + '!');
 export default nameUser;
